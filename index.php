a2449a72e8f160cbc0615641f55710aca59a65dd
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-25 16:59:19 +0200

    Parola censurata

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php 
-    $stringa = "Questa è una stringa di prova";
+    $stringa = "L'esercizio consiste nel censurare una parola. La parola censurare dev'essere sostituita da tre asterischi.";
+    echo $stringa;
     echo strlen($stringa);
+
+    $_GET["censura"];
+    $stringa_censurata = str_replace('censura', '***', $stringa)
 ?>
